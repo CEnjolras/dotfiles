ee94f201bd3e03e2429a759b2a0909d8a60de7f6
Clement Enjolras <enj.clement@gmail.com>
2023-07-29 18:07:52 +0200

    Updated script

--- install.sh
+++ install.sh
@@ -29,36 +29,37 @@ info_print () {
 info_print "Setup pacman & yay : fastest mirrors, enable colors, parallel downloads, system update..."
 
 # Setting up reflector
-# pacman -S --noconfirm reflector
-# reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
-# systemctl enable reflector.timer
+info_print "Setting up reflector..."
+sudo pacman -S --needed --noconfirm reflector && sudo systemctl enable --now reflector.timer && sudo reflector --latest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
 
 # Enabling colors and setting up parallelDownloads
-# sed -Ei 's/^#(Color)$/\1\n#ILoveCandy/;s/^#(ParallelDownloads).*/\1 = 10/' /etc/pacman.conf
+info_print "Enabling colors and parallel downloads..."
+sed -Ei 's/^#(Color)$/\1\n#ILoveCandy/;s/^#(ParallelDownloads).*/\1 = 10/' /etc/pacman.conf
 
 # System upgrade
-#pacman -Syyu
+info_print "System upgrade..."
+pacman -Syyu
 
 # Installing yay
+info_print "Installing yay..."
 pacman -S --noconfirm --needed git base-devel
-git clone https://aur.archlinux.org/yay.git $HOME/yay 
-pushd $HOME/yay
-su - $ADMIN -c "makepkg -si"
-popd
-rm -rf $HOME/yay
+git clone https://aur.archlinux.org/yay.git && (cd yay && makepkg -si --noconfirm) && rm -rf yay
+
 
 # Clearing cache from time to time
-#pacman -S --noconfirm pacman-contrib
-#curl -L --create-dirs -o $HOME/.local/bin/yaycache https://bit.ly/yaycache && chmod +x $HOME/.local/bin/yaycache
-#touch $HOME/.bashrc && echo 'export PATH="$HOME//.local/bin:$PATH"' >> $HOME/.bashrc && source $HOME/.bashrc
+sudo pacman -S --noconfirm pacman-contrib
+curl -L --create-dirs -o $HOME/.local/bin/yaycache https://bit.ly/yaycache && chmod +x $HOME/.local/bin/yaycache
+[ -f "$HOME/.bashrc" ] && grep -qxF 'export PATH="$HOME/.local/bin:$PATH"' "$HOME/.bashrc" || echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.bashrc" && source "$HOME/.bashrc"
+
 
 
 #====================
 # 1 - Git
 #====================
 
-#git config --global user.name  "Clément Enjolras"
-#git config --global user.email "enj.clement@gmail.com"
+git config --global user.name  "Clément Enjolras"
+git config --global user.email "enj.clement@gmail.com"
 
 
 
