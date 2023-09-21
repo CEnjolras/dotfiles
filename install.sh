5e382c73712c38ed3edcd0a2f9277730c165fcd7
Clement Enjolras <enj.clement@gmail.com>
2023-09-21 19:48:59 +0200

    Install ansible and wget, update playbook download command

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 # Setups arch linux for me.
 #
 
-# 1 - Installing ansible
+# 1 - Installing ansible and wget
 #sudo pacman -S ansible --noconfirm
 
 # 2 - Downloading and playing ansible playbook
-wget https://raw.githubusercontent.com/CEnjolras/dotfiles/main/playbook.yml playbook.yml
+curl -o playbook.yml https://raw.githubusercontent.com/CEnjolras/dotfiles/main/playbook.yml
 ansible-playbook playbook.yml --ask-become-pass
