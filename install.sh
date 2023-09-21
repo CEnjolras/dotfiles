12745e155739efb3a3922dd0450b597a285880e0
Clement Enjolras <enj.clement@gmail.com>
2023-09-22 00:32:51 +0200

    Update curl command in install.sh to include "Cache-Control: no-cache" header while downloading main.yml file

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@
 #sudo pacman -S ansible --noconfirm
 
 # 2 - Downloading and playing ansible playbook
-curl -o main.yml https://raw.githubusercontent.com/CEnjolras/dotfiles/main/main.yml
+curl -o main.yml -H "Cache-Control: no-cache" https://raw.githubusercontent.com/CEnjolras/dotfiles/main/main.yml
 ansible-playbook main.yml --ask-become-pass
